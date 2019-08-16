4d365d2f74f30a66d7f8b7b9974ca8ab313f4000
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-08-15 22:00:40 -0500

    Prolog

---
+++ Matching_de_parejas.pl
@@ -0,0 +1,315 @@
+hombre(juan).
+deportes(juan,[natacion, futbol, voleibol]).
+peliculas(juan,[aventura, comedia, drama]).
+musica(juan,[blues, corrido, country, cumbia]).
+hobbies(juan,[leer, ver_tv, pasar_tiempo_con_la_familia, pescar, pasear]).
+comida(juan,[pizza, hamburguesas, tacos, mole, pescado, barbacoa]).
+lugares(juan, [cine, gym, museo, restaurantes, plaza, zoologico, teatro, parque, centro_de_artes, conciertos]).
+bebidas(juan,[agua, refresco, cerveza, mezcal, ron, vodka, ginebra, jugo]).
+personalidad(juan,[responsable, educado, repetuoso, ordenado, espontaneo, amable]).
+viajes(juan,[cancun, ciudad_de_mexico, chichen_itza]).
+
+
+hombre(david).
+deportes(david,[futbol, voleibol, baloncesto, tenis, beisbol, balonmano, basquet, ciclismo, gimansia, ski]).
+peliculas(david,[aventura,  accion, crimen]).
+musica(david,[blues, corrido, country, cumbia, tango, reggaeton]).
+hobbies(david,[leer, ver_tv, hacer_ejercicio, ir_de_caza, dormir, viajar, ver_partidos, cantar]).
+comida(david,[pizza, mole, posole, tortas, enchiladas, guizados, barbacoa]).
+lugares(david, [cine, museo, restaurantes, conciertos]).
+bebidas(david,[ ron, vino, tequila, whisky]).
+personalidad(david,[responsable, educado, divertido, enojon, consistente]).
+viajes(david,[cancun, ciudad_de_mexico, morelia]).
+
+hombre(jose).
+deportes(jose,[futbol, basquet, ciclismo]).
+peliculas(jose,[terror, musicales, ciencia_ficcion, guerra, accion, crimen]).
+musica(jose,[heavy_metal, hip_hop, pop, rock, salsa, tango, reggaeton]).
+hobbies(jose,[dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(jose,[tacos, mole, posole, tortas, enchiladas, guizados, mole]).
+lugares(jose, [zoologico, teatro, parque, centro_de_artes, conciertos]).
+bebidas(jose,[ron, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(jose,[extrovertido, divertido, enojon, consistente]).
+viajes(jose,[palenque, guanajuato, morelia]).
+
+hombre(angel).
+deportes(angel,[tenis, beisbol, balonmano, basquet]).
+peliculas(angel,[drama, terror, musicales, ciencia_ficcion, guerra]).
+musica(angel,[heavy_metal, hip_hop, pop, rock, salsa]).
+hobbies(angel,[leer, ir_de_caza, dormir, viajar,  videojuegos, cantar]).
+comida(angel,[pizza, pescado, barbacoa]).
+lugares(angel, [oologico, teatro, parque, centro_de_artes]).
+bebidas(angel,[aron, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(angel,[responsable, educado, repetuoso, ordenado, enojon, consistente]).
+viajes(angel,[cancun,  chichen_itza, tulum, puerto_vallarta,  morelia]).
+
+hombre(marco).
+deportes(marco,[tenis, beisbol, balonmano, basquet, ciclismo, gimansia, ski]).
+peliculas(marco,[aventura, comedia, drama, terror, musicales, ciencia_ficcion]).
+musica(marco,[blues, corrido,  electronica, heavy_metal, salsa, tango, reggaeton]).
+hobbies(marco,[leer, pescar, pasear, hacer_ejercicio, cocinar, videojuegos, actuar]).
+comida(marco,[pizza, tacos, mole, posole, quesadillas]).
+lugares(marco, [cine, teatro, parque, centro_de_artes, conciertos]).
+bebidas(marco,[agua, refresco, cerveza, jugo]).
+personalidad(marco,[ordenado, espontaneo, amable, consistente]).
+viajes(marco,[cancun, sumidero,guanajuato, morelia]).
+
+hombre(arturo).
+deportes(arturo,[natacion, futbol, voleibol, baloncesto, tenis, beisbol, balonmano, basquet, ciclismo, gimansia, ski]).
+peliculas(arturo,[aventura, comedia, drama, terror, musicales, ciencia_ficcion, guerra, accion, crimen]).
+musica(arturo,[blues, corrido, country, cumbia, disco, electronica, heavy_metal, hip_hop, pop, rock, salsa, tango, reggaeton]).
+hobbies(arturo,[leer, ver_tv, pasar_tiempo_con_la_familia, pescar, pasear, hacer_ejercicio, ir_de_caza, dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(arturo,[pizza, hamburguesas, tacos, mole, posole, tortas, enchiladas, guizados, mole, quesadillas, pescado, barbacoa]).
+lugares(arturo, [cine, gym, museo, restaurantes, plaza, zoologico, teatro, parque, centro_de_artes, conciertos]).
+bebidas(arturo,[agua, refresco, cerveza, mezcal, ron, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(arturo,[responsable, educado, repetuoso, ordenado, espontaneo, amable, extrovertido, divertido, enojon, consistente]).
+viajes(arturo,[cancun, ciudad_de_mexico, chichen_itza, tulum, puerto_vallarta, sumidero, palenque, guanajuato, morelia]).
+
+hombre(alexis).
+deportes(alexis,[futbol, tenis, beisbol, basquet]).
+peliculas(alexis,[drama, terror, musicales, ciencia_ficcion, guerra, accion, crimen]).
+musica(alexis,[heavy_metal, hip_hop, rock]).
+hobbies(alexis,[pasear, hacer_ejercicio, dormir, viajar, ver_partidos, videojuegos]).
+comida(alexis,[pizza, hamburguesas, tacos, quesadillas, pescado]).
+lugares(alexis, [museo, restaurantes,parque, conciertos]).
+bebidas(alexis,[agua, refresco, cerveza]).
+personalidad(alexis,[espontaneo, amable, extrovertido, divertido, consistente]).
+viajes(alexis,[tulum, puerto_vallarta,  morelia]).
+
+hombre(julio).
+deportes(julio,[natacion, futbol, voleibol, baloncesto, tenis, beisbol, basquet, ciclismo]).
+peliculas(julio,[aventura,  drama, musicales, guerra, accion, crimen]).
+musica(julio,[country, cumbia, disco, hip_hop, pop, rock, reggaeton]).
+hobbies(julio,[ver_tv, pasar_tiempo_con_la_familia, ir_de_caza, dormir, viajar, cocinar, videojuegos, cantar]).
+comida(julio,[hamburguesas, mole, posole, enchiladas, pescado, barbacoa]).
+lugares(julio, [cine, gym, museo, restaurantes, plaza, teatro, parque]).
+bebidas(julio,[cerveza, mezcal, ron, tequila, jugo]).
+personalidad(julio,[educado, repetuoso, ordenado, espontaneo, amable, extrovertido, divertido]).
+viajes(julio,[cancun, ciudad_de_mexico, chichen_itza, tulum, puerto_vallarta, sumidero, palenque, guanajuato, morelia]).
+
+hombre(mario).
+deportes(mario,[natacion, voleibol, baloncesto, tenis, ciclismo, gimansia, ski]).
+peliculas(mario,[accion, crimen]).
+musica(mario,[ heavy_metal, hip_hop, pop, rock, salsa, tango, reggaeton]).
+hobbies(mario,[leer, ver_tv, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(mario,[pizza,  posole, tortas, enchiladas, guizados, pescado, barbacoa]).
+lugares(mario, [cine, gym,  teatro, parque, centro_de_artes, conciertos]).
+bebidas(mario,[agua, cerveza,  whisky, vodka, ginebra, jugo]).
+personalidad(mario,[responsable, divertido, enojon, consistente]).
+viajes(mario,[cancun, ciudad_de_mexico, chichen_itza, tulum, puerto_vallarta, morelia]).
+
+hombre(eduardo).
+deportes(eduardo,[natacion]).
+peliculas(eduardo,[musicales]).
+musica(eduardo,[electronica]).
+hobbies(eduardo,[cantar]).
+comida(eduardo,[pizza]).
+lugares(eduardo, [conciertos]).
+bebidas(eduardo,[agua]).
+personalidad(eduardo,[responsable]).
+viajes(eduardo,[guanajuato]).
+
+
+mujer(mary).
+deportes(mary,[futbol, voleibol, tenis, beisbol, balonmano, gimansia, ski]).
+peliculas(mary,[aventura, comedia, drama, musicales, ciencia_ficcion]).
+musica(mary,[blues, cumbia, disco, pop, rock, salsa, tango, reggaeton]).
+hobbies(mary,[leer,  pasar_tiempo_con_la_familia, pescar, ir_de_caza, dormir, viajar, cocinar,  actuar, cantar]).
+comida(mary,[guizados, mole, quesadillas, pescado, barbacoa]).
+lugares(mary, [cine, museo, restaurantes, plaza, zoologico, teatro]).
+bebidas(mary,[agua, refresco, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(mary,[responsable, enojon, consistente]).
+viajes(mary,[cancun, ciudad_de_mexico, palenque, guanajuato, morelia]).
+
+mujer(julieta).
+deportes(julieta,[natacion, gimansia, ski]).
+peliculas(julieta,[drama, terror, guerra, accion, crimen]).
+musica(julieta,[corrido, heavy_metal, hip_hop, rock]).
+hobbies(julieta,[pasear, hacer_ejercicio, ir_de_caza]).
+comida(julieta,[barbacoa]).
+lugares(julieta, [gym, conciertos]).
+bebidas(julieta,[cerveza, mezcal, tequila]).
+personalidad(julieta,[extrovertido,  enojon, consistente]).
+viajes(julieta,[chichen_itza, tulum, sumidero, palenque]).
+
+mujer(jimena).
+deportes(jimena,[natacion, futbol, voleibol, baloncesto, tenis, beisbol, balonmano, basquet, ciclismo, gimansia, ski]).
+peliculas(jimena,[aventura, comedia, drama, terror, musicales, ciencia_ficcion, guerra, accion, crimen]).
+musica(jimena,[blues, corrido, country, cumbia, disco, electronica, heavy_metal, hip_hop, pop, rock, salsa, tango, reggaeton]).
+hobbies(jimena,[leer, ver_tv, pasar_tiempo_con_la_familia, pescar, pasear, hacer_ejercicio, ir_de_caza, dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(jimena,[pizza, hamburguesas, tacos, mole, posole, tortas, enchiladas, guizados, mole, quesadillas, pescado, barbacoa]).
+lugares(jimena, [cine, gym, museo, restaurantes, plaza, zoologico, teatro, parque, centro_de_artes, conciertos]).
+bebidas(jimena,[agua, refresco, cerveza, mezcal, ron, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(jimena,[responsable, educado, repetuoso, ordenado, espontaneo, amable, extrovertido, divertido, enojon, consistente]).
+viajes(jimena,[cancun, ciudad_de_mexico, chichen_itza, tulum, puerto_vallarta, sumidero, palenque, guanajuato, morelia]).
+
+mujer(paola).
+deportes(paola,[natacion, futbol, voleibol, tenis,  basquet]).
+peliculas(paola,[aventura,  musicales, ciencia_ficcion, crimen]).
+musica(paola,[blues, salsa, tango, reggaeton]).
+hobbies(paola,[leer,  dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(paola,[pizza, hamburguesas, tacos]).
+lugares(paola, [cine, gym]).
+bebidas(paola,[agua, refresco, cerveza, mezcal, ron, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(paola,[responsable, divertido, enojon, consistente]).
+viajes(paola,[cancun, ciudad_de_mexico, morelia]).
+
+mujer(pilar).
+deportes(pilar,[natacion, futbol, voleibol, baloncesto, tenis, beisbol, balonmano, basquet, ciclismo, gimansia, ski]).
+peliculas(pilar,[aventura, comedia, drama, terror, musicales, ciencia_ficcion, guerra, accion, crimen]).
+musica(pilar,[blues, corrido, country, cumbia, disco, electronica, heavy_metal, hip_hop, pop, rock, salsa, tango, reggaeton]).
+hobbies(pilar,[leer, ver_tv, pasar_tiempo_con_la_familia, pescar, pasear, hacer_ejercicio, ir_de_caza, dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(pilar,[pizza, hamburguesas, tacos, mole, posole, tortas, enchiladas, guizados, mole, quesadillas, pescado, barbacoa]).
+lugares(pilar, [cine, gym, museo, restaurantes, plaza, zoologico, teatro, parque, centro_de_artes, conciertos]).
+bebidas(pilar,[agua, refresco, cerveza, mezcal, ron, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(pilar,[responsable, educado, repetuoso, ordenado, espontaneo, amable, extrovertido, divertido, enojon, consistente]).
+viajes(pilar,[cancun, ciudad_de_mexico, chichen_itza, tulum, puerto_vallarta, sumidero, palenque, guanajuato, morelia]).
+
+mujer(andrea).
+deportes(andrea,[beisbol, balonmano,  gimansia, ski]).
+peliculas(andrea,[aventura, comedia, drama]).
+musica(andrea,[blues, cumbia, disco, electronica, tango]).
+hobbies(andrea,[leer, pasear, hacer_ejercicio, ir_de_caza, dormir, cocinar,  actuar, cantar]).
+comida(andrea,[pizza, mole, posole, mole, quesadillas, pescado, barbacoa]).
+lugares(andrea, [cine, gym, museo, restaurantes, plaza, teatro, parque, centro_de_artes]).
+bebidas(andrea,[agua, refresco, tequila, whisky, vodka]).
+personalidad(andrea,[responsable, educado, extrovertido, divertido, consistente]).
+viajes(andrea,[cancun, ciudad_de_mexico, tulum, puerto_vallarta, sumidero, morelia]).
+
+mujer(yoceline).
+deportes(yoceline,[natacion, futbol, voleibol, baloncesto, tenis, beisbol,  basquet, ciclismo, gimansia]).
+peliculas(yoceline,[aventura, comedia, drama, terror, musicales, ciencia_ficcion,  crimen]).
+musica(yoceline,[blues, corrido, country, cumbia, disco, electronica, heavy_metal, hip_hop, pop, rock, salsa, tango,reggaeton]).
+hobbies(yoceline,[leer, ver_tv, pasear, hacer_ejercicio, ir_de_caza, dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(yoceline,[pizza, hamburguesas, tacos, mole,  tortas, enchiladas, quesadillas, pescado, barbacoa]).
+lugares(yoceline, [cine, gym, restaurantes, plaza, zoologico,  conciertos]).
+bebidas(yoceline,[agua, refresco, cerveza, mezcal, tequila, whisky, jugo]).
+personalidad(yoceline,[educado, repetuoso, ordenado, extrovertido, divertido, enojon, consistente]).
+viajes(yoceline,[tulum, puerto_vallarta, sumidero, palenque, guanajuato, morelia]).
+
+mujer(karla).
+deportes(karla,[beisbol, balonmano, basquet, ciclismo, gimansia, ski]).
+peliculas(karla,[ciencia_ficcion, guerra, accion, crimen]).
+musica(karla,[heavy_metal, hip_hop, pop, rock, salsa, tango, reggaeton]).
+hobbies(karla,[hacer_ejercicio, ir_de_caza, dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(karla,[ guizados, mole, quesadillas, pescado, barbacoa]).
+lugares(karla, [teatro, parque, centro_de_artes, conciertos]).
+bebidas(karla,[ron, vino, tequila, whisky, vodka, ginebra, jugo]).
+personalidad(karla,[ espontaneo, amable, extrovertido, divertido, enojon, consistente]).
+viajes(karla,[guanajuato, morelia]).
+
+mujer(cindy).
+deportes(cindy,[natacion, futbol, voleibol, baloncesto, tenis]).
+peliculas(cindy,[aventura, comedia, drama, terror, musicales]).
+musica(cindy,[blues, corrido, country, cumbia, disco, electronica]).
+hobbies(cindy,[leer, ver_tv, pasar_tiempo_con_la_familia, pescar, pasear]).
+comida(cindy,[pizza, hamburguesas, tacos, mole, posole, tortas]).
+lugares(cindy, [cine, gym, museo, restaurantes, plaza, zoologico]).
+bebidas(cindy,[agua, refresco, cerveza, mezcal, ron, vino, tequila]).
+personalidad(cindy,[responsable, educado, repetuoso, ordenado, espontaneo]).
+viajes(cindy,[cancun, ciudad_de_mexico, chichen_itza, tulum, puerto_vallarta]).
+
+mujer(tania).
+deportes(tania,[natacion, futbol, ciclismo, gimansia, ski]).
+peliculas(tania,[aventura, comedia, guerra, accion, crimen]).
+musica(tania,[blues, corrido, country, salsa, tango, reggaeton]).
+hobbies(tania,[leer, ver_tv, dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+comida(tania,[pizza, hamburguesas, tacos, pescado, barbacoa]).
+lugares(tania, [cine, gym, museo, centro_de_artes, conciertos]).
+bebidas(tania,[agua, refresco, vodka, ginebra, jugo]).
+personalidad(tania,[responsable, educado, repetuoso, consistente]).
+viajes(tania,[cancun, ciudad_de_mexico, chichen_itza, guanajuato, morelia]).
+
+%   CRITERIOS   %
+
+%deportes(nombre,[natacion, futbol, voleibol, baloncesto, tenis, beisbol, balonmano, basquet, ciclismo, gimansia, ski]).
+%peliculas(nombre,[aventura, comedia, drama, terror, musicales, ciencia_ficcion, guerra, accion, crimen]).
+%musica(nombre,[blues, corrido, country, cumbia, disco, electronica, heavy_metal, hip_hop, pop, rock, salsa, tango, reggaeton]).
+%hobbies(nombre,[leer, ver_tv, pasar_tiempo_con_la_familia, pescar, pasear, hacer_ejercicio, ir_de_caza, dormir, viajar, ver_partidos, cocinar, videojuegos, actuar, cantar]).
+%comida(nombre,[pizza, hamburguesas, tacos, mole, posole, tortas, enchiladas, guizados, mole, quesadillas, pescado, barbacoa]).
+%lugares(nombre, [cine, gym, museo, restaurantes, plaza, zoologico, teatro, parque, centro_de_artes, conciertos]).
+%bebidas(nombre,[agua, refresco, cerveza, mezcal, ron, vino, tequila, whisky, vodka, ginebra, jugo]).
+%personalidad(nombre,[responsable, educado, repetuoso, ordenado, espontaneo, amable, extrovertido, divertido, enojon, consistente]).
+%viajes(nombre,[cancun, ciudad_de_mexico, chichen_itza, tulum, puerto_vallarta, sumidero, palenque, guanajuato, morelia])
+
+largo([],0).
+largo([H|T],N):-largo(T,N0), N is N0 + 1.
+
+member(X,[X|T]).
+member(X,[_|T]):-member(X,T).
+
+co([],L2,0).
+co([X|T],L2,C):-member(X,L2),co(T,L2,C2),C is C2 + 1.
+co([X|T],L2,C):-not(member(X,L2)),co(T,L2,C2),C is C2.
+
+
+pareja(H,M,Porcentaje,Porc_matching):-hombre(H),mujer(M),
+                        deportes(H,Deportesh),deportes(M,Deportesm),
+                        peliculas(H,Peliculash),peliculas(M,Peliculasm),
+                        musica(H,Musicah),musica(M,Musicam),
+                        hobbies(H,Hobbiesh),hobbies(M,Hobbiesm),
+                        comida(H,Comidah),comida(M,Comidam),
+                        lugares(H,Lugaresh),lugares(M,Lugaresm),
+                        bebidas(H,Bebidash),bebidas(M,Bebidasm),
+                        personalidad(H,Perh),personalidad(M,Perm),
+                        viajes(H,Viajesh),viajes(M,Viajesm),
+
+                        co(Deportesh,Deportesm,Cdeportes),
+                        co(Peliculash,Peliculasm,Cpeliculas),
+                        co(Musicah,Musicam,Cmusica),
+                        co(Hobbiesh,Hobbiesm,Chobbies),
+                        co(Comidah,Comidam,Ccomida),
+                        co(Lugaresh,Lugaresm,Clugares),
+                        co(Bebidash,Bebidasm,Cbebidas),
+                        co(Perh,Perm,Cper),
+                        co(Viajesh,Viajesm,Cviajes),
+
+
+                        largo(Deportesh,Ldeportes1),largo(Deportesm,Ldeportes2),
+                        largo(Peliculash,Lpeliculas1),largo(Peliculasm,Lpeliculas2),
+                        largo(Musicah,Lmusica1),largo(Musicam,Lmusica2),
+                        largo(Hobbiesh,Lhobbies1),largo(Hobbiesm,Lhobbies2),
+                        largo(Comidah,Lcomida1),largo(Comidam,Lcomida2),
+                        largo(Lugaresh,Llugares1),largo(Lugaresm,Llugares2),
+                        largo(Bebidash,Lbebidas1),largo(Bebidasm,Lbebidas2),
+                        largo(Perh,Lper1),largo(Perm,Lper2),
+                        largo(Viajesh,Lviajes1),largo(Viajesm,Lviajes2),
+
+
+                        DeportesAmbos is Ldeportes1+Ldeportes2,
+                        PeliculasAmbos is Lpeliculas1+Lpeliculas2,
+                        MusicaAmbos is Lmusica1+Lmusica2,
+                        HobbiesAmbos is Lhobbies1+Lhobbies2,
+                        ComidaAmbos is Lcomida1+Lcomida2,
+                        LugaresAmbos is Llugares1+Llugares2,
+                        BebidasAmbos is Lbebidas1+Lbebidas2,
+                        PerAmbos is Lper1+Lper2,
+                        ViajesAmbos is Lviajes1+Lviajes2,
+
+
+                        Prom_deportes is DeportesAmbos/2,
+                        Prom_peliculas is PeliculasAmbos/2,
+                        Prom_musica is MusicaAmbos/2,
+                        Prom_hobbies is HobbiesAmbos/2,
+                        Prom_comida is ComidaAmbos/2,
+                        Prom_lugares is LugaresAmbos/2,
+                        Prom_bebidas is BebidasAmbos/2,
+                        Prom_per is PerAmbos/2,
+                        Prom_viajes is ViajesAmbos/2,
+
+
+                        Porc_matching_deportes is ((Cdeportes/Prom_deportes)*100),
+                        Porc_matching_peliculas is ((Cpeliculas/Prom_peliculas)*100),
+                        Porc_matching_musica is ((Cmusica/Prom_musica)*100),
+                        Porc_matching_hobbies is ((Chobbies/Prom_hobbies)*100),
+                        Porc_matching_comida is ((Ccomida/Prom_comida)*100),
+                        Porc_matching_lugares is ((Clugares/Prom_lugares)*100),
+                        Porc_matching_bebidas is ((Cbebidas/Prom_bebidas)*100),
+                        Porc_matching_per is ((Cper/Prom_per)*100),
+                        Porc_matching_viajes is ((Cviajes/Prom_viajes)*100),
+
+                        Ptotal is Porc_matching_deportes+Porc_matching_peliculas+Porc_matching_musica+Porc_matching_hobbies+Porc_matching_comida+Porc_matching_lugares+Porc_matching_bebidas+Porc_matching_per+Porc_matching_viajes,
+
+
+                        Porc_matching is Ptotal/9,
+                        Porc_matching>=Porcentaje.
